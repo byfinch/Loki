1fe29876c43bcc2fa0c411aaeb0fed21ee57eb96
Efsunlu Kemal <efsunlukemal@gmail.com>
2026-08-14 07:43:01 +0300

    erisim sadece domain uzerinden: nginx localhost'a kilitlendi, IP origin'leri kaldirildi

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
       env: {
         NODE_ENV: 'production',
         PORT: 3001,
-        LOKI_ALLOWED_ORIGINS: 'http://localhost:4173,http://127.0.0.1:4173',
+        LOKI_ALLOWED_ORIGINS: 'https://phishguard.click,https://www.phishguard.click,http://localhost:4173,http://127.0.0.1:4173',
         LOKI_TELEGRAM_BOT_TOKEN: '',
         LOKI_TELEGRAM_CHAT_ID: '',
         LOKI_PHISH_DB_PATH: '/opt/phishguard/data/phishguard.db',
@@ -16,7 +16,7 @@ module.exports = {
       env_production: {
         NODE_ENV: 'production',
         PORT: 3001,
-        LOKI_ALLOWED_ORIGINS: 'http://localhost:4173,http://127.0.0.1:4173',
+        LOKI_ALLOWED_ORIGINS: 'https://phishguard.click,https://www.phishguard.click,http://localhost:4173,http://127.0.0.1:4173',
         LOKI_TELEGRAM_BOT_TOKEN: '',
         LOKI_TELEGRAM_CHAT_ID: '',
         LOKI_PHISH_DB_PATH: '/opt/phishguard/data/phishguard.db',
